76a9e226e57ea38d9a01df400b2bb9e5aa3588e8
chinztwo <115656265+chinztwo@users.noreply.github.com>
2022-10-14 19:06:39 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const load = document.querySelector('.load');
 const content = document.querySelector('.wrapper');
+const message = document.querySelector('.message');
+const generate = document.querySelector('.generate');
+const toggf = document.querySelectorAll('.tggl');
+const waveshad = document.querySelectorAll('.wave');
+// ^-----Variables-----^
+
+
+//Loading Web Page
+
 window.addEventListener('load',()=>{
     setTimeout(function(){
         load.classList.add('fade');
@@ -8,14 +17,12 @@ window.addEventListener('load',()=>{
             load.classList.add('hide');
             content.classList.remove('fade');
         },1000)
-    },5000)
+    },3500)
 })
 
-
-const message = document.querySelector('.message');
-const generate = document.querySelector('.generate');
+//Generate Quote. Random index for array from .JSON derulo
 generate.addEventListener('click', () => {
-    let rand = Math.floor(Math.random() * (30 + 1));
+    let rand = Math.floor(Math.random() * (31 + 1));
     message.classList.add('fade');
     setTimeout(function(){
         readTextFile("jason derulo/msg.json", function (text) {
@@ -27,13 +34,12 @@ generate.addEventListener('click', () => {
 
 });
 
-//message.innerHTML = msgs[rand];
 
 
 
 
 
-//ReadTextFile from StackOverflow
+//ReadTextFile function from StackOverflow...
 function readTextFile(file, callback) {
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
@@ -45,7 +51,30 @@ function readTextFile(file, callback) {
     }
     rawFile.send(null);
 }
-
+var x = 1; //Index value for darkmode
+const toggle = document.querySelector('.toggle-dark');
+toggle.addEventListener('click',()=>{
+    readTextFile("jason derulo/svgswap.json", function (swap) {
+        var swapsvg = JSON.parse(swap);
+        x = x == 2? 0 : 1;
+        toggle.innerHTML = swapsvg[x];
+    })
+    ++x;
+    darkmode();
+    console.log(x);
+})
 //tranisiton
+function darkmode(){
+    for(var i = 0; i < toggf.length; i++){
+        toggf[i].classList.toggle('dark');
+    }
+    for(var i = 0; i < waveshad.length; i++){
+        waveshad[i].classList.toggle('wave-shad');
+    }
+}
 
+
+function lightmode(){
+
+}
 
